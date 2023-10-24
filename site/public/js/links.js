d4b6624458d6a5d5f55ce6119fafc6187a3b553a
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2023-10-24 17:49:01 -0300

    Adicionando links na navbar da página home

---
+++ site/public/js/links.js
@@ -0,0 +1,15 @@
+const linkCalculadora = document.getElementById('link_cal').addEventListener('click', () => {
+    window.location.href = '#section_calculator'
+});
+
+const linkSobre = document.getElementById('link_abt').addEventListener('click', () => {
+    window.location.href = '#section_about'
+});
+
+const linkhome = document.getElementById('link_home').addEventListener('click', () => {
+    window.location.href = '#home'
+});
+
+const linkLogin = document.getElementById('link_login').addEventListener('click', () => {
+    window.location.href = 'login_signup.html'
+});
