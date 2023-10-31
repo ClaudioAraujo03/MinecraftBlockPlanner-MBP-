1fac0f6942b69213859e2dc8d173d0f65e91e0a7
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2023-10-31 19:30:07 -0300

    validando as entradas das inputs na tela de cadastro

---
+++ site/public/js/loginCadastrar.js
@@ -0,0 +1,93 @@
+var nomeInput = document.getElementById('nome_input')
+var emailInput = document.getElementById('email_input')
+var senhaInput = document.getElementById('senha_input')
+var nickInput = document.getElementById('nick_input')
+var dtNascInput = document.getElementById('dtNasc_input') 
+
+const btnCadastrar = document.getElementById('btn_cadastro').addEventListener('click', () => {
+    var nomeVar = nomeInput.value;
+    var emailVar = emailInput.value;
+    var senhaVar = senhaInput.value;
+    var nickVar = nickInput.value;
+    var dtNascVar = dtNascInput.value;
+
+    var spanValidData = document.getElementById('valid_span');
+    
+    nomeInput.placeholder = '';
+    emailInput.placeholder = '';
+    senhaInput.placeholder = '';
+    nickInput.placeholder = '';
+    spanValidData.innerText = '';
+
+
+    const regexNum = /[0-9]/;
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const regexLow = /[a-z]/;
+    const regexUp = /[A-Z]/;
+
+    var erros = 0
+
+    if(nomeVar == '' || regexNum.test(nomeVar)){
+        nomeInput.value = '';
+        nomeInput.placeholder = 'Nome inválido';
+        erros++;
+    }
+    if(emailVar == '' || !regexEmail.test(emailVar)){
+        emailInput.value = '';
+        emailInput.placeholder = 'Email inválido';
+        erros++;
+    }
+    if(senhaVar == ''){
+        senhaInput.value = '';
+        senhaInput.placeholder = 'Senha inválida';
+        erros++;
+    } else if(!regexNum.test(senhaVar) || !regexLow.test(senhaVar) || !regexUp.test(senhaVar) || senhaVar.length < 8){
+        senhaInput.value = '';
+        senhaInput.placeholder = 'Senha deve ter a-z, A-Z, 0-9 e tamanho 8 ';
+        erros++;
+    }
+    if(nickVar == '' || nickVar.length < 5){
+        nickInput.value = '';
+        nickInput.placeholder = 'Nick inválido';
+        erros++;
+    }
+    if(dtNascVar == ''){
+        spanValidData.innerText = 'Data inválida';
+        erros++;
+    }
+
+    if(erros == 0){
+        fetch("/login/cadastrar", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nomeServer: nomeVar,
+            emailServer: emailVar,
+            senhaServer: senhaVar,
+            nickServer: nickVar,
+            dtNascServer: dtNascVar
+          }),
+        })
+          .then(function (resposta) {
+            console.log("resposta: ", resposta);
+    
+            if (resposta.ok) {
+              alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...")
+    
+              setTimeout(() => {
+                window.location = "loginSignup.html";
+              }, "2000");
+    
+            } else {
+              throw "Houve um erro ao tentar realizar o cadastro!, erro no HTML";
+            }
+          })
+          .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+          });
+    
+        return false;
+    }
+})
