0bdac0e1525006253ec3feb00a701246dff171d6
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2023-10-31 16:18:10 -0300

    Remoção da calculadora e adição de mais cards sobre o site

--- site/public/js/nav.js
+++ site/public/js/nav.js
@@ -1,11 +1,3 @@
-const linkCalculadora = document.getElementById('link_cal').addEventListener('click', () => {
-    window.location.href = '#section_calculator'
-});
-
-const linkCalculadoraBtn = document.getElementById('link_cal_btn').addEventListener('click', () => {
-    window.location.href = '#section_calculator'
-});
-
 const linkSobre = document.getElementById('link_abt').addEventListener('click', () => {
     window.location.href = '#section_about'
 });
