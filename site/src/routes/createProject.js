995bf97b172c06b5757d14595c0a55a6ad572bb3
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2023-11-05 18:30:30 -0300

    criando a pasta com as imagens dos icones dos blocos

--- site/src/routes/createProject.js
+++ site/src/routes/createProject.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+var controllerCreateProject = require('../controllers/controllerCreateProject')
+
 router.get('/criarProjeto', function(req, res){
     res.render('dashboard/createProject')
+});
+
+router.post('/criarProjeto/:idUsuario', function(req, res){
+    controllerCreateProject.criarProjeto(req, res);
 });
 
 module.exports = router;
